eebde4c3c96a370e0eec58a136334f11fe0cd0b0
Spencer Kohan <spencerkohan@gmail.com>
2018-10-01 15:55:26 +0200

    added swift 4.2 support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,5 +24,6 @@ let package = Package(
         .testTarget(
             name: "EventEmitterTests",
             dependencies: ["EventEmitter"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v4, .v4_2]
 )
