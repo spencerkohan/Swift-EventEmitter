7004b8bef4b9f199ed7eaa530598644ab543942a
Spencer Kohan <spencerkohan@gmail.com>
2022-07-29 16:31:55 +0200

    updated swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,6 +24,5 @@ let package = Package(
         .testTarget(
             name: "EventEmitterTests",
             dependencies: ["EventEmitter"]),
-    ],
-    swiftLanguageVersions: [.v4, .v4_2, .v5]
+    ]
 )
